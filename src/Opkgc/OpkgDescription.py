48d70f632bf52a1b7187fa39989951df202e6437
jparpail <jparpail@18f9d06c-6ae2-0310-8438-938a554feff9>
2007-07-18 12:45:19 +0000

    [OPKGC] 	Real handling of arch filters, for packages. 	Closes #321
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/pkgsrc@6025 18f9d06c-6ae2-0310-8438-938a554feff9

--- src/Opkgc/OpkgDescription.py
+++ src/Opkgc/OpkgDescription.py
@@ -11,26 +11,20 @@ from time import *
 from OpkgcXml import *
 from OpkgcTools import *
 
-__all__ = ['OpkgDescription',
-           'OpkgDescriptionDebian',
-           'OpkgDescriptionRpm',
-           'OpkgSyntaxException']
-
 class OpkgDescription:
     """ Contains data for templates
     """
-    oscarDepends = {"clientDeps":"oscar-base-client",
-                    "apiDeps":"oscar-base",
-                    "serverDeps":"oscar-base-server"}    
     xmldoc = None
     month = {"01":"Jan", "02":"Feb", "03":"Mar", "04":"Apr",
              "05":"May", "06":"Jun", "07":"Jul", "08":"Aug",
              "09":"Sep", "10":"Oct", "11":"Nov", "12":"Dec"}
     dist = ""
+    depsFactory = None
 
     def __init__(self, xmldoc, dist):
         self.xmldoc = xmldoc
         self.dist = dist
+        self.depsFactory = DependsFactory(xmldoc)
 
     def date(self, date, format):
         """ Convert 'xsdDate' in xsd:dateTime format
@@ -112,42 +106,17 @@ class OpkgDescription:
             author += ' (%s)' % nickname
         author += ' <%s>' % etree.findtext('email')
         return author
-        
-    def formatDepends(self, dlist, part, relation):
-        """ Format a dependancy list element
-        (requires|provides|conflicts|suggests).
-        relation: dlist parent
-        part: relation parent
-        (lxml does not keep track of parents :-(
-        """
-        pkglist = []
-        for d in dlist:
-            pkglist.extend(d.findall('pkg'))
 
-        depends = ''
-        i = 0
-        # Add requires for oscar-base-<part>
-        if relation == 'requires':
-            depends += self.oscarDepends[part]
-            i += 1
-            
-        for pkg in pkglist:
-            if i != 0:
-                depends += ', '
-            depends += self.formatPkg(pkg)
-            i += 1
-        ret = ''
-        if i == 0:
-            return ''
-        else:
-            return self.dependsName[relation] + ': ' + depends + '\n'
-
-    def filterDist(self, elem, dist):
-        """ Return true if 'elem' contains a filter for distribution 'dist'
-        or no filters at all.
+    def getArchs(self):
+        """ Return list of authorized arch filters from config.xml XML schema
         """
-        distFilters = [d.text.strip().lower() for d in elem.findall('filters/dist')]
-        return len(distFilters) == 0 or dist in distFilters
+        schema = XmlTools().getXsdDoc()
+        complexType = filter(lambda x: x.get("name") == "archType",
+                             schema.findall("{%s}simpleType" % XmlTools.xsd_uri))[0]
+        archs = [e.get("value")
+                 for e in complexType.find("{%s}restriction" % XmlTools.xsd_uri).findall("{%s}enumeration" % XmlTools.xsd_uri)]
+
+        return archs
 
 class OpkgDescriptionRpm(OpkgDescription):
     """ Filters out some fields in a opkg description,
@@ -157,12 +126,8 @@ class OpkgDescriptionRpm(OpkgDescription):
                    "conflicts":"Conflicts",
                    "provides":"Provides"}
 
-    relName = {"<":"<",
-               "<=":">=",
-               "=":"=",
-               ">=":">=",
-               ">":">"}
-
+    relName = KeyDict()
+    
     fileList = []
     scripts = {}
 
@@ -188,23 +153,41 @@ class OpkgDescriptionRpm(OpkgDescription):
     def depends(self, part, relation):
         """ Return list of dependencies of type 'relation' for
         the 'part' package part.
-        Relation is one of: requires, conflicts, provides, suggests
+        Relation is one of: requires, conflicts, provides
         Part is one of: apiDeps, serverDeps, clientDeps
         """
-        dlist = [d
-                 for d in self.xmldoc.findall(part + '/' + relation)
-                 if self.filterDist(d, self.dist)]
-        return self.formatDepends(dlist, part, relation)
+        archs = self.getArchs()
+        archs.append(None)
 
-    def formatPkg(self, e):
-        """ Return formatted package name plus dependancy relation
+        out = ""
+        for arch in archs:
+            deps = []
+            deps.extend(self.depsFactory.getDeps(part=part, relation=relation, arch=arch, dist=None))
+            deps.extend(self.depsFactory.getDeps(part=part, relation=relation, arch=arch, dist=self.dist))
+
+            if len(deps) != 0:
+                archout = ""
+                if arch != None:
+                    archout += "%%ifarch %s\n" % arch
+                archout += "%s: " % self.dependsName[relation]
+                for i, d in enumerate(deps):
+                    if i != 0:
+                        archout += ', '
+                    archout += self.formatPkg(d)
+                archout += "\n"
+                if arch != None:
+                    archout += "%endif\n"
+                out += archout
+
+        return out
+
+    def formatPkg(self, p):
+        """ Return formatted package dep
         """
-        ret = e.text.strip()
-        version = e.get('version')
-        rel = e.get('rel')
-        if version:
-            ret += ' %s %s' % (self.relName[rel], version)
-        return ret
+        out = p['name']
+        if p['version']:
+            out += ' %s %s' % (self.relName[p['op']], p['version'])
+        return out
 
     def fileList(self):
         return self.fileList
@@ -233,20 +216,15 @@ class OpkgDescriptionDebian(OpkgDescription):
         for Debian templates
     """
 
-    archName = {"i386":"i386",
-                "amd64":"amd64",
-                "x86_64":"ia64"}
+    archName = KeyDict({"x86_64":"ia64"})
 
     dependsName = {"requires":"Depends",
                    "conflicts":"Conflicts",
                    "provides":"Provides",
                    "suggests":"Suggests"}
 
-    relName = {"<":"<<",
-               "<=":">=",
-               "=":"=",
-               ">=":">=",
-               ">":">>"}
+    relName = KeyDict({"<":"<<",
+                       ">":">>"})
 
     def description(self):
         """ Return the description in Debian format:
@@ -275,10 +253,23 @@ class OpkgDescriptionDebian(OpkgDescription):
         Relation is one of: requires, conflicts, provides, suggests
         Part is one of: apiDeps, serverDeps, clientDeps
         """
-        dlist = [d
-                 for d in self.xmldoc.findall(part + '/' + relation)
-                 if self.filterDist(d, self.dist)]
-        return self.formatDepends(dlist, part, relation)
+        deps = []
+        deps.extend(self.depsFactory.getDeps(part=part, relation=relation, arch="*", dist=None))
+        deps.extend(self.depsFactory.getDeps(part=part, relation=relation, arch="*", dist='debian'))
+
+        if len(deps) == 0:
+            return ""
+        else:
+            out = "%s: " % self.dependsName[relation]
+            for i, d in enumerate(deps):
+                if i != 0:
+                    out += ', '
+                out += self.formatPkg(d)
+            out += "\n"
+
+        Logger().debug(out)
+
+        return out
 
     def changelog(self):
         """ Return a list of versionEntries
@@ -347,15 +338,20 @@ class OpkgDescriptionDebian(OpkgDescription):
 
         return copyrights
 
-    def formatPkg(self, e):
-        """ Return formatted package name plus dependancy relation
+    def formatPkg(self, p):
+        """ Return formatted package information for Debian
         """
-        ret = e.text.strip()
-        version = e.get('version')
-        rel = e.get('rel')
-        if version:
-            ret += ' (%s %s)' % (self.relName[rel], version)
-        return ret
+        out = p['name']
+        if p['version']:
+            out += " (%s %s)" % (self.relName[p['op']], p['version'])
+        if len(p['arch']) != 0:
+            out += " ["
+            for i, a in enumerate(p['arch']):
+                if i != 0:
+                    out += " "
+                out += "%s" % a
+            out += "]"
+        return out
 
 class OpkgSyntaxException(Exception):
 
@@ -363,3 +359,145 @@ class OpkgSyntaxException(Exception):
 
     def __init__(self, msg):
         self.msg = msg
+
+class DependsFactory(object):
+    """ Store an retrieve package dependancy informations
+    """
+    partsReqs = {"clientDeps":"oscar-base-client",
+                 "apiDeps":"oscar-base",
+                 "serverDeps":"oscar-base-server"}
+
+    packageDeps = None
+    filters = None
+
+    def __init__(self, xmldoc):
+        self.packageDeps = []
+        self.filters = []
+        self.__loadFromXml(xmldoc)
+
+        # Add OSCAR automatic deps
+        for part in self.partsReqs.keys():
+            p = PackageDeps(name=self.partsReqs[part],
+                            relation='requires',
+                            part=part)
+            self.packageDeps.append(p)
+
+    def __loadFromXml(self, xmldoc):
+        """ Load packageDeps from xmldoc
+        """
+        for pn in self.getPartNames():
+            part = xmldoc.find("/%s" % pn)
+            if part:
+                for rn in self.getRelationNames():
+                    relations = part.findall('%s' % rn)
+                    for r in relations:
+                        (dists, archs) = self.getFilters(r.find('filters'))
+                        self.filters.extend(dists)
+                        self.filters.extend(archs)
+                    
+                        pkgList = r.findall("pkg")
+                        for pkg in pkgList:
+                            p = PackageDeps(name=pkg.text,
+                                            op=pkg.get("rel"),
+                                            version=pkg.get("version"),
+                                            arch=archs,
+                                            dist=dists,
+                                            relation=rn,
+                                            part=pn)
+                            self.packageDeps.append(p)
+
+    def getFilters(self, e):
+        """ Return 2 lists: dist and arch filters of
+        an filters element
+        """
+        dists = []
+        archs = []
+
+        if e:
+            for df in e.findall('dist'):
+                d = DistFilter(name=df.text,
+                               op=df.get("rel"),
+                               version=df.get("version"))
+                dists.append(d)
+            for af in e.findall('arch'):
+                archs.append(af.text)
+
+        return (dists, archs)
+        
+
+    def getPartNames(self):
+        schema = XmlTools().getXsdDoc()
+        return [e.get("name")
+                for e in filter(lambda x: x.get("type") == "depsListType",
+                                schema.findall('/{%s}element' % XmlTools.xsd_uri))]
+
+    def getRelationNames(self):
+        schema = XmlTools().getXsdDoc()
+        complexType = filter(lambda x: x.get("name") == "depsListType",
+                             schema.findall('{%s}complexType' % XmlTools.xsd_uri))[0]
+        return [e.get("ref")
+                for e in complexType.find('{%s}sequence' % XmlTools.xsd_uri).findall('{%s}element' % XmlTools.xsd_uri)]
+
+    def getDeps(self, relation, part, arch=None, dist=None):
+        """ Return list of PackageDeps matching the given criteria
+        arch can be '*' and return all archs
+        """
+        ret = []
+
+        for p in self.packageDeps:
+            if p['relation'] !=  relation:
+                continue
+            if p['part'] != part:
+                continue
+            if arch != '*':
+                if (arch and not arch in p['arch']) \
+                       or (not arch and len(p['arch']) > 0):
+                    continue
+            if (dist and dist not in [d['name'] for d in p['dist']]) \
+                   or (not dist and len(p['dist']) > 0):
+                continue
+
+            ret.append(p)
+                        
+        return ret
+
+class DistFilter(NoneDict):
+
+    def __init__(self, name, op=None, version=None):
+        NoneDict.__init__(self)
+        self['name'] = name
+        self['op'] = op
+        self['version'] = version
+
+    def __str__(self):
+        out = self['name']
+        if self['op']:
+            out += "%s%s" % (self['op'], self['version'])
+        return out
+
+class PackageDeps(NoneDict):
+
+    def __init__(self, name, relation, part, op=None, version=None, arch=[], dist=[]):
+        NoneDict.__init__(self)
+        self['name'] = name
+        self['arch'] = arch
+        self['dist'] = dist
+        self['relation'] = relation
+        self['op'] = op
+        self['version'] = version
+        self['part'] = part
+
+    def __str__(self):
+        out = "%s %s %s" % (self['part'], self['relation'], self['name'])
+        if self['version']:
+            out += " (%s%s)" % (self['op'], self['version'])
+        if len(self['dist']) > 0:
+            out += " for distro "
+            for d in self['dist']:
+                out += "%s, " % d
+        if len(self['arch']):
+            out += " for arch "
+            for a in self['arch']:
+                out += "%s, " % a
+
+        return out
