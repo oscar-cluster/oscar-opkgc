64f55e9fa2e40a418ef5bae70c2e51b452aef90a
olahaye74 <olahaye74@18f9d06c-6ae2-0310-8438-938a554feff9>
2013-04-07 08:56:21 +0000

    [opkgc: build_rpm.sh] Script fixes.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/pkgsrc@9824 18f9d06c-6ae2-0310-8438-938a554feff9

--- build_rpm.sh
+++ build_rpm.sh
@@ -2,13 +2,12 @@
 ./autogen.sh
 ./configure
 make dist
-rpmbuild -tb ./opkgc-*.tar.gz
 RPMS="$(LC_ALL=C rpmbuild -tb opkgc-1.0.1.tar.gz |grep ^Wrote:|cut -d' ' -f2)"
 
 if test -d "$PKGDEST"
 then
-	echo "  --> Moving ${RPMS/\n/ /} to ${PKGDEST}/"
-	mv -f ${RPMS/\n/ /} ${PKGDEST}/
+	echo "  --> Moving ${RPMS} to ${PKGDEST}/"
+	mv -f ${RPMS} ${PKGDEST}/
 fi
 
 # remove the Makefile to prevent for a second build attempt (make rpm) that would fail
